eaff7bd9bc980d5d2ba4d1f71de9c89166803eaa
starplayer632 <98778169+starplayer632@users.noreply.github.com>
2022-09-07 15:36:56 +0200

    Added jwt auth token system

--- server/index.js
+++ server/index.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const app = express()
 const cors = require('cors') //Just for dev not neede in production
-const PORT = process.env.PORT || '1337';
 const dotenv = require('dotenv');
 dotenv.config({path: '.env-local'});
+const jwt = require('jsonwebtoken')
 
+const PORT = process.env.PORT || '1337';
+/**
+ * Testing
+ */
 
+const posts = [
+	{
+	  username: 'Kyle',
+	  title: 'Post 1'
+	},
+	{
+	  username: 'Jim',
+	  title: 'Post 2'
+	}
+]
 
+app.get('/posts', authenticateToken, (req, res) => {
+	res.json(posts.filter(post => post.username === req.user.name))
+})
+
+function authenticateToken(req, res, next) {
+	const authHeader = req.headers['authorization']
+	const token = authHeader && authHeader.split(' ')[1]
+	if (token == null) return res.sendStatus(401)
+  
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+	  console.log(err)
+	  if (err) return res.sendStatus(403)
+	  req.user = user
+	  next()
+	})
+}
 
 /** 
  * Middleware
@@ -23,6 +53,10 @@ app.use('/api/users/students/', user_studentsRouter);
 
 const mongoDBRouter= require("./routes/mongodb_routes");
 app.use('/api/mongodb/', mongoDBRouter);
+
+const jwtauth= require("./routes/jwtauth");
+app.use('/api/jwtauth/', jwtauth);
+
 
 //Test Hello World
 app.get('/hello', (req, res) => {
