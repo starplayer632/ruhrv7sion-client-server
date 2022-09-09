ee1c5e0d0ec556a060456d4318e3a94e1dea38be
Isztof <mariuszseget77@gmail.com>
2022-09-09 22:02:25 +0200

    Fixing compilation error

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,58 @@
-import React from 'react'
+import React from "react";
 //react Router for Routing
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 //loading bootstrap min local
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //import pages
-import Login from './pages/auth/Login'
-import Register from './pages/auth/Register'
-import Error404 from './pages/Error404'
-import Dashboard from './pages/Dashboard'
-import Home from './pages/studentpage/LandingStudent'
-import Header from './components/Header.js';
-import Footer from './components/Footer.js';
-import Jobs from './pages/Jobs.js';
-import NewJobOffer from './pages/NewJobOffer.js';
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Error404 from "./pages/Error404";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/studentpage/LandingStudent";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import Jobs from "./pages/Jobs.js";
+import NewJobOffer from "./pages/NewJobOffer.js";
 
 //App as const -> as a function
 const App = () => {
-	
-	//hasJWT does the user has a token?
-	function hasJWT() {
-		let flag = false;
-  
-		//check user has JWT token
-		localStorage.getItem("token") ? flag=true : flag=false
-	   
-		return flag
-	}
+  //hasJWT does the user has a token?
+  function hasJWT() {
+    let flag = false;
 
-	return (
-		<div className="App" style={{
-			backgroundColor:'#f5f5f5',
-		}}>
-			{/** Routing wihtout ROLES */}
-			<BrowserRouter>
-			{/** For now Header and Footer will stay the same until ROLES and Persistent User Login Authentication with JWT Tokens is been solved*/}
-				<Header />
-				<Routes>
-					<Route path="/" exact element={<Home />} />
-					<Route path="/login" exact element={<Login />} />
-					<Route path="/jobs" exact element={<Jobs />} />
-					<Route path="/dashboard" exact element={<Dashboard />} />
-					<Route path="/register" exact element={<Register />} />
-					<Route path="/jobs/newJobOffer" exact element={<NewJobOffer />} />
+    //check user has JWT token
+    localStorage.getItem("token") ? (flag = true) : (flag = false);
 
+    return flag;
+  }
 
-					<Route path="*" exact element={<Error404 />} />
-				</Routes>
-				
-				<Footer />
-			</BrowserRouter>
-			
-				
-			
-		</div>
-	)
-}
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor: "#f5f5f5",
+      }}
+    >
+      {/** Routing wihtout ROLES */}
+      <BrowserRouter>
+        {/** For now Header and Footer will stay the same until ROLES and Persistent User Login Authentication with JWT Tokens is been solved*/}
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="./pages/Auth/Login" exact element={<Login />} />
+          <Route path="/jobs" exact element={<Jobs />} />
+          <Route path="/dashboard" exact element={<Dashboard />} />
+          <Route path="./pages/Auth/Register" exact element={<Register />} />
+          <Route path="/jobs/newJobOffer" exact element={<NewJobOffer />} />
 
-export default App
+          <Route path="*" exact element={<Error404 />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
